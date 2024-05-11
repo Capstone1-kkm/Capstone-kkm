f92c0384c4456562e74b9bc315f498a80ac54b7b
Kimdaeun0929 <daeun5802@naver.com>
2024-05-11 17:36:39 +0900

    firebase Auth 추가

--- app/src/main/java/com/example/capstone/mypage.java
+++ app/src/main/java/com/example/capstone/mypage.java
@@ -8,12 +8,20 @@ import android.widget.TextView;
 
 public class mypage extends Activity {
 
+    TextView greetingTextView;
+    TextView loginTextView;
+
+    boolean isLoggedIn = false;
+    String loggedInUsername = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mypage);
 
         TextView textView = findViewById(R.id.login1);
+        greetingTextView = findViewById(R.id.login2);
+        loginTextView = findViewById(R.id.login1);
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -21,8 +29,27 @@ public class mypage extends Activity {
                 // 예를 들어, 새로운 화면으로 이동하는 코드를 작성할 수 있습니다.
                 Intent intent = new Intent(mypage.this, login1.class);
                 startActivity(intent);
-
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1){
+            if(resultCode == RESULT_OK){
+                loggedInUsername = data.getStringExtra("username");
+                isLoggedIn = true;
+                updateGreeting();
+            }
+        }
+    }
+    private void updateGreeting(){
+        if(isLoggedIn){
+            greetingTextView.setText(loggedInUsername + "님, 안녕하세요");
+        }
+        else{
+            greetingTextView.setText(("비회원님, 안녕하세요"));
+        }
     }
 }
