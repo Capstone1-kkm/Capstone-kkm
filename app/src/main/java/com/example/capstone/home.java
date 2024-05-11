2bf18769a27f06231398379fd66bdddb17397b48
MoonYoonJae <boroyun@naver.com>
2024-05-11 23:11:43 +0900

    버튼들 intent 일부 추가

--- app/src/main/java/com/example/capstone/home.java
+++ app/src/main/java/com/example/capstone/home.java
@@ -15,7 +15,20 @@ public class home extends AppCompatActivity {
         setContentView(R.layout.home);
 
         // 뒤로가기 버튼 눌렀을때 이전 스택에 쌓인 액티비티로 이동하게 됨
-        ImageView imageView = findViewById(R.id.imageViewBottom5);
+        ImageView imageView;
+        imageView = findViewById(R.id.imageViewBottom2);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 새로운 액티비티로 이동하는 Intent를 생성합니다.
+                Intent intent = new Intent(home.this, googlemap.class);
+                // Intent를 사용하여 새로운 액티비티로 이동합니다.
+                startActivity(intent);
+            }
+        });
+
+        // 뒤로가기 버튼 눌렀을때 이전 스택에 쌓인 액티비티로 이동하게 됨
+        imageView = findViewById(R.id.imageViewBottom5);
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -26,20 +39,6 @@ public class home extends AppCompatActivity {
             }
         });
 
-       /* // AAA 이미지뷰를 찾습니다.
-        ImageView imageView = findViewById(R.id.imageView);
-
-        // AAA 이미지뷰에 OnClickListener를 설정합니다.
-        imageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // 새로운 액티비티로 이동하는 Intent를 생성합니다.
-                Intent intent = new Intent(home.this, store1.class);
-
-                // Intent를 사용하여 새로운 액티비티로 이동합니다.
-                startActivity(intent);
-            }
-        });*/
     }
 }
 
--- app/src/main/java/com/example/capstone/mypage.java
+++ app/src/main/java/com/example/capstone/mypage.java
@@ -41,17 +41,39 @@ public class mypage extends Activity {
         isLoggedIn = prefs.getBoolean(KEY_LOGGED_IN, false);
         updateGreeting();
 
-        // 뒤로가기 버튼 눌렀을 때 이전 스택에 쌓인 액티비티로 이동
+        // 뒤로가기 버튼 눌렀을때 이전 스택에 쌓인 액티비티로 이동하게 됨
         ImageView imageView = findViewById(R.id.imageViewBottom1);
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        // 뒤로가기 버튼 눌렀을때 이전 스택에 쌓인 액티비티로 이동하게 됨
+        imageView = findViewById(R.id.imageViewBottom2);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 // 새로운 액티비티로 이동하는 Intent를 생성합니다.
-                Intent intent = new Intent(mypage.this, login1.class);
+                Intent intent = new Intent(mypage.this, googlemap.class);
                 // Intent를 사용하여 새로운 액티비티로 이동합니다.
                 startActivity(intent);
             }
         });
+
+        // 뒤로가기 버튼 눌렀을때 이전 스택에 쌓인 액티비티로 이동하게 됨
+        imageView = findViewById(R.id.imageViewBottom3);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 새로운 액티비티로 이동하는 Intent를 생성합니다.
+                Intent intent = new Intent(mypage.this, home.class);
+                // Intent를 사용하여 새로운 액티비티로 이동합니다.
+                startActivity(intent);
+            }
+        });
+
     }
 
     @Override
